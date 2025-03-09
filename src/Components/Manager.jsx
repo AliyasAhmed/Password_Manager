28b28f700a3af28494ed7a273fa4756b073d85d1
unknown <iliyaaszahoor@gmail.com>
2025-03-09 19:14:34 +0530

    Another react Project

--- src/Components/Manager.jsx
+++ src/Components/Manager.jsx
@@ -1,8 +1,16 @@
-import { React, useRef, useState } from "react";
+import { React, useRef, useState, useEffect } from "react";
 import VisibleEye from "../assets/svg/VisibleEye.svg";
 import NotVisibleEye from "../assets/svg/VisibleOffeye.svg";
 const Manager = () => {
-    const [Form, SetForm] = useState({website:"",username:"",password:""})
+  useEffect(() => {
+    let passwords = localStorage.getItem("password");
+    let passwordArray;
+    if (passwords) {
+      setpasswordArray(JSON.parse(passwords));
+    }
+  }, []);
+  const [Form, SetForm] = useState({ website: "", username: "", password: "" });
+  const [passwordArray, setpasswordArray] = useState([]);
   const ref = useRef();
   const Showpassword = () => {
     if (ref.current.src.includes(NotVisibleEye)) {
@@ -11,16 +19,13 @@ const Manager = () => {
       ref.current.src = NotVisibleEye;
     }
   };
-  const SavePassWord = () =>{
-    let password = localStorage.getItem("password", password)
-    if (password){
-        localStorage.setItem(password)
-    }
-
-  }
-  const handlechange = (e) =>{
-    SetForm({...Form, [e.target.name]:e.target.value})
-  }
+  const SavePassWord = () => {
+    setpasswordArray([...passwordArray, Form])
+    localStorage.setItem('password', JSON.stringify([...passwordArray, Form]))
+  };
+  const handlechange = (e) => {
+    SetForm({ ...Form, [e.target.name]: e.target.value });
+  };
   return (
     <>
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
@@ -28,7 +33,7 @@ const Manager = () => {
         <p className="text-2xl">Your own paswword manager</p>
         <div className="text-white flex flex-col p-4 gap-3">
           <input
-            className="border border-purple-600 w-full rounded-lg text-black p-3"
+            className="bg-[#3f3f3f27] text-white w-full rounded-lg p-3 focus:border-green-600"
             type="text"
             name="website"
             id=""
@@ -37,25 +42,25 @@ const Manager = () => {
             onChange={handlechange}
           />
           <div className="flex w-full gap-8">
-          <input
-            className="border border-purple-600 w-full rounded-lg text-black p-3"
-            type="text"
-            name="username"
-            id=""
-            placeholder="Enter Username"
-            value={Form.username}
-            onChange={handlechange}
-          />
-            <div className="relative">
             <input
-            className="border border-purple-600 w-full rounded-lg text-black p-3"
-            type="text"
-            name="password"
-            id=""
-            placeholder="Enter password"
-            value={Form.password}
-            onChange={handlechange}
-          />
+              className=" w-full rounded-lg bg-[#3f3f3f27] text-white p-3"
+              type="text"
+              name="username"
+              id=""
+              placeholder="Enter Username"
+              value={Form.username}
+              onChange={handlechange}
+            />
+            <div className="relative">
+              <input
+                className=" w-full rounded-lg bg-[#3f3f3f27] text-white p-3"
+                type="text"
+                name="password"
+                id=""
+                placeholder="Enter password"
+                value={Form.password}
+                onChange={handlechange}
+              />
               <span className="absolute right-0 text-purple-700 top-[0.8em] px-1 cursor-pointer">
                 <img src={VisibleEye} alt="" onClick={Showpassword} ref={ref} />
               </span>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const navbar = () => {
   return (
     <nav className=" text-white flex justify-center p-5">
-        <div className="font-bold text-2xl hover:scale-[90%] transition-all cursor-pointer">
+        <div className="font-bold text-5xl hover:scale-[90%] transition-all cursor-pointer">
             <span className="text-purple-700">&lt;</span>
             Pass
             <span className="text-purple-700">wrd /&gt;</span>
