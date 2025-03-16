55cec084fcd97584bf6918d5f0bada0a26d60015
unknown <iliyaaszahoor@gmail.com>
2025-03-16 14:02:47 +0530

    still working

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-const navbar = () => {
+const Navbar = () => {
   return (
     <nav className=" text-white flex justify-center p-5">
         <div className="font-bold text-5xl hover:scale-[90%] transition-all cursor-pointer">
-            <span className="text-purple-700">&lt;</span>
+            <span className="text-[#2516c7b7]">&lt;</span>
             Pass
-            <span className="text-purple-700">wrd /&gt;</span>
+            <span className="text-[#2516c7b7]">wrd /&gt;</span>
         </div>
     </nav>
   );
 };
 
-export default navbar;
+export default Navbar;
